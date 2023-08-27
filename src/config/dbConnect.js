08813df2f486669dc406bc83b597c015d1ec0d56
igordscunha <124375584+igordscunha@users.noreply.github.com>
2023-08-27 04:46:30 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://USERHERE:lIxYJB9oUVr7nWgJ@dbcl.3qfmtns.mongodb.net/DB_COLLECT")
+mongoose.connect("mongodb+srv://USERHERE:PWHERE@dbcl.3qfmtns.mongodb.net/DB_COLLECT")
 
 let db = mongoose.connection
 
